648a56b86cecb867770a44d0cc42e0198f2836b8
Kasturee Nayak <kasturee1998@gmail.com>
2024-04-04 11:48:26 +0530

    Classroom Application

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -19,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("students")
 public class StudentController {
 
+    @Autowired
+    StudentService studentService=new StudentService();
+
     @PostMapping("/add-student")
     public ResponseEntity<String> addStudent(@RequestBody Student student){
 
@@ -40,28 +43,28 @@ public class StudentController {
     @GetMapping("/get-student-by-name/{name}")
     public ResponseEntity<Student> getStudentByName(@PathVariable String name){
         Student student = null; // Assign student by calling service layer method
-
+        student = studentService.findStudent(name);
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-teacher-by-name/{name}")
     public ResponseEntity<Teacher> getTeacherByName(@PathVariable String name){
         Teacher teacher = null; // Assign student by calling service layer method
-
+        teacher = studentService.findTeacher(name);
         return new ResponseEntity<>(teacher, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-students-by-teacher-name/{teacher}")
     public ResponseEntity<List<String>> getStudentsByTeacherName(@PathVariable String teacher){
         List<String> students = null; // Assign list of student by calling service layer method
-
+        students = studentService.findStudentsFromTeacher(teacher);
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-all-students")
     public ResponseEntity<List<String>> getAllStudents(){
         List<String> students = null; // Assign list of student by calling service layer method
-
+        students = studentService.findAllStudents();
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,70 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        String name = student.getName();
+        studentMap.put(name, student);
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        String name = teacher.getName();
+        teacherMap.put(name, teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
             // your code goes here
+            List<String> students = teacherStudentMapping.get(teacher);
+            students.add(student);
+            teacherStudentMapping.put(teacher, students);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        Student name = studentMap.get(student);
+        return name;
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        Teacher name=teacherMap.get(teacher);
+        return name;
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        List<String> listOfStudents=teacherStudentMapping.get(teacher);
+        return listOfStudents;
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        List<String> studentList = new ArrayList<>();
+        for(String student : studentMap.keySet()){
+            studentList.add(student);
+        }
+        return studentList;
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        for(String student : teacherStudentMapping.get(teacher)){
+            studentMap.remove(student);
+        }
+        teacherMap.remove(teacher);
+        teacherStudentMapping.remove(teacher);
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        for(String teacher : teacherMap.keySet()){
+            for(String student : teacherStudentMapping.get(teacher)){
+                studentMap.remove(student);
+            }
+            teacherMap.remove(teacher);
+            teacherStudentMapping.remove(teacher);
+        }
     }
 }
